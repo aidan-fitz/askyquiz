05661b7c832b2293ff193bbeaa09d2e2df6c4881
suds-p <ssss.pontula@gmail.com>
2018-10-20 21:36:01 -0400

    Initial commit

---
+++ authors.mli
@@ -0,0 +1,12 @@
+(**
+   Metadata about this submission.
+   @author
+        Aidan Fitzgerald (acf67),
+        Karen Zhou (kz265),
+        Sudeshna Pontula (slp256),
+        Yue Li (yl2282) 
+*)
+
+(** [hours_worked] is a list of the number of hours each team member
+    worked on this assignment. *)
+val hours_worked : int list
