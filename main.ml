a69d867e5f89fdad2ab0fb404f24aedc67b2d7f7
vagrant <vagrant@vm3110-2018fa>
2018-10-22 23:01:14 -0400

    added basic funcs to main

---
+++ main.ml
@@ -0,0 +1,22 @@
+open Quiz
+open Progress
+open Yojson
+open Yojson.Basic
+open Yojson.Basic.Util
+
+(** [load_quiz f] is the [Quiz] object created from the quiz
+    JSON with filename [f]*)
+let load_quiz f = 
+  try 
+    let j = from_file f in
+    Some (parse_json j)
+  with 
+  | Sys_error _ -> print_endline "File not found"; None
+  | Json_error _ -> print_endline "File does not contain valid JSON"; None
+  | Type_error _ -> print_endline "JSON does not represent adventure"; None
+
+(** [main ()] prompts for the game to play, then starts it. *)
+let main () = ()
+
+(* Execute the game engine. *)
+let () = main ()
