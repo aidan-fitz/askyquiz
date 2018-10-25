6aa47e30b4a2361fca40ea11653a75c74a4e9d0b
Sudeshna Pontula <slp256@cornell.edu>
2018-10-24 21:19:43 -0400

    Alternate letter sequences

--- main.ml
+++ main.ml
@@ -15,28 +15,35 @@ let load_quiz f =
   | Json_error _ -> print_endline "File does not contain valid JSON"; None
   | Type_error _ -> print_endline "JSON does not represent adventure"; None
 
-let answer_letters_odd  = ["A"; "B"; "C"; "D"; "E"]
-let answer_letters_even = ["F"; "G"; "H"; "J"; "K"]
+let odd_letters  = ["A"; "B"; "C"; "D"; "E"]
+let even_letters = ["F"; "G"; "H"; "J"; "K"]
 
-(** [ask qn quiz] displays [qn] to the screen and prompts for an answer
-    among its choices in [quiz]. *)
-let ask (qid, qs) quiz = 
-  print_endline qs;
+(** [ask qn is_odd quiz] displays [qn] to the screen and prompts for an answer
+    among its choices in [quiz]. Answers are enumerated with A, B, C, D, E if 
+    [is_odd is true], and with F, G, H, J, K otherwise. *)
+let ask (qid, qtxt) is_odd quiz = 
+  print_endline qtxt;
+
   (* print up to 5 answers; if there are fewer than 5, catch the exception *)
   try List.iter2
       (fun (_, ans) letter -> print_endline (letter ^ ". " ^ ans))
       (get_answers qid quiz)
-      answer_letters_odd
+      (if is_odd then odd_letters else even_letters)
   with Invalid_argument _ -> ();
-  print_string "Answer: ";
-  let input = read_line () in
-  print_endline ("You answered " ^ input ^ "\n")
+
+    print_string "Answer: ";
+    let input = read_line () in
+    print_endline ("You answered " ^ input ^ "\n")
 
 (** [run_quiz q] plays through quiz [q] with the user. *)
 let run_quiz quiz =
+  let is_odd = ref true in
   let rec run = function
     | [] -> ()
-    | q::qs -> ask q quiz; run qs
+    | q::qs ->
+        ask q !is_odd quiz;
+        is_odd := not !is_odd;
+        run qs
   in run (get_questions quiz)
 
 (** [main ()] prompts for the game to play, then starts it. *)
