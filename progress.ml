6cfec0edd0a6602b90d72e1844d6413ef37b19e4
Aidan Fitzgerald <acf67@cornell.edu>
2018-10-26 21:46:00 -0400

    Write basic requeue logic

--- progress.ml
+++ progress.ml
@@ -13,6 +13,11 @@ let init_progress quiz = {
   score = List.map (fun x -> (x, ref 0)) (Quiz.categories quiz) 
 }
 
+(** [do_requeue s] tells whether to requeue the most recently answered question
+    based on [s]. *)
+let do_requeue scores =
+  List.assoc "correct" scores = 0
+
 (** [update_progress qid aid quiz prog] updates [prog] to reflect the user
     answering with ID [aid] for question with ID [qid]. Running scores in 
     [prog] are updated according to the answer's scores in [quiz]. *)
@@ -26,7 +31,7 @@ let update_progress qid aid quiz prog =
     prog.score;
   {
     queue = List.filter ((<>) qid) prog.queue;
-    requeue = qid :: prog.requeue; (* TODO add logic for requeuing if incorrect *)
+    requeue = if do_requeue scores then qid :: prog.requeue else prog.requeue;
     score = prog.score;
   }
 
