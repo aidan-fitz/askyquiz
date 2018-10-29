68044237da0028627c0a90cec6d5bb9595e292a3
Sudeshna Pontula <slp256@cornell.edu>
2018-10-29 15:47:14 -0400

    Refactor main

--- main.ml
+++ main.ml
@@ -6,43 +6,43 @@ open Yojson.Basic.Util
 open QCheck
 open ANSITerminal
 
-(** [mode] *)
+(** The current testing mode. *)
 type mode = Subjective | Test | Practice
 
-(** [load_quiz f] is the [Quiz.t] created from the quiz JSON with filename [f]. 
-    If [f] is not a valid quiz, it prompts for another input. *)
+(** [load_quiz f] is the [Quiz.t] created from the quiz JSON in file [f]. 
+    If the JSON does not represent a valid quiz, it reprompts for a file. *)
 let rec load_quiz () = 
   let f = read_line () in
   let quiz = 
-    try 
-      let j = from_file f in
-      Some (parse_json j)
+    try let j = from_file f in Some (parse_json j)
     with 
-    | Sys_error _ -> print_string [yellow] "File not found. "; None
-    | Json_error _ -> print_string [yellow] 
-                        "File does not contain valid JSON. "; None
+    | Sys_error _  -> print_string [yellow] "File not found. "; None
+    | Json_error _ -> print_string [yellow]
+                      "File does not contain valid JSON. "; None
     | Type_error _ -> print_string [yellow] 
-                        "JSON does not represent quiz. "; None
+                      "JSON does not represent quiz. "; None
   in match quiz with 
   | Some q -> q
-  | None -> print_string [yellow] "Try again:";
-    print_newline ();
+  | None -> 
+    print_string [yellow] "Try again:\n";
     print_string [] "> ";
     load_quiz ()
 
-let odd_letters  = ["A"; "B"; "C"; "D"; "E"]
-let even_letters = ["F"; "G"; "H"; "J"; "K"]
+(* let odd_letters  = ["A"; "B"; "C"; "D"; "E"]
+let even_letters = ["F"; "G"; "H"; "J"; "K"] *)
 
-(** [make_ltrs qid is_odd quiz] is the list of letter choices appropriate to 
-    the corresponding question *)
-let make_ltrs qid is_odd quiz =
-  (** [ltrs lst acc n] truncates [lst] to a length of n *)
-  let rec ltrs lst acc n =
-    if n = 0 then acc 
-    else ltrs (List.tl lst) ((List.hd lst) :: acc) (n - 1) in
-  let ltr_list = (if is_odd then odd_letters else even_letters) in
-  let l = List.length (answers quiz qid) in
-  List.rev (ltrs ltr_list [] l)
+(** [next l] is the letter after [l] in the alphabet. *)
+let next l = Char.(chr ((code l - code 'A' + 1) mod 26 + code 'A'))
+
+(** [make_letters n is_odd] is a list of [n] letters starting from 'A' if 
+    [is_odd] and starting from 'F' otherwise. The sequence always skips 'I'. *)
+let make_letters n is_odd =
+  let rec go n l acc =
+    if n = 0 then acc else
+    if (next l) = 'I' 
+    then go (n-1) (l |> next |> next) ((String.make 1 l)::acc)
+    else go (n-1) (l |> next) ((String.make 1 l)::acc)
+  in if is_odd then go n 'A' [] else go n 'F' []
 
 (** [shuffle letters lst] returns association lists where the key is a 
     letter option and the values are an answer id in the shuffled list 
@@ -74,7 +74,7 @@ let imm_feedback correct_aid correct options =
 let requeue qid mstry correct =
   let m = List.assoc qid mstry in
   if correct then let () = m := !m + 1 in (not (m = ref 3))
-  else let () = (if (not (m = ref 0)) then (m := !m - 1) else ()) in true
+  else let () = (if (not (m = ref 0)) then (m := !m - 1)) in true
 
 (** [check_answer qid aid mode] updates progress and gives feedback according 
     to [mode]*)
@@ -111,8 +111,8 @@ let rec ask q is_odd mode quiz prog =
     print_newline ();
     print_endline qtxt;
 
-    let ltrs = make_ltrs qid is_odd quiz in 
     let ans_pairs = (get_answers qid quiz) in
+    let ltrs = make_letters (List.length ans_pairs) is_odd in 
     let options = shuffle ltrs ans_pairs in
     (* print up to 5 answers; if there are fewer than 5, catch the exception *)
     List.iter (fun (ltr, (id, ans)) -> 
