f3d3000788f043bba7e4ea2f7a750cc273b09b0e
Sudeshna Pontula <slp256@cornell.edu>
2018-10-31 16:05:15 -0400

    Fix comparison in stock test

--- testProgress.ml
+++ testProgress.ml
@@ -8,7 +8,8 @@ let i = init_progress quiz1
 let progress_tests = [
   (*TODO: writes tests for quiz*)
   "initial stock" >:: (fun  _ ->
-    assert_equal (stock i) ["q1"; "q2"; "q3"; "q4"; "q5"]);
+    assert_equal (stock i) ["q1"; "q2"; "q3"; "q4"; "q5"]
+    ~cmp:cmp_set_like_lists);
   "initial discard pile" >:: (fun _ -> assert_equal (discard i) []);
   "initial score" >:: (fun _ ->
     assert_equal (score i) [("correct", ref 0)]
