b7bd4467f2c5721375b2b8267100eddcef53b1f2
Sudeshna Pontula <slp256@cornell.edu>
2018-11-04 14:28:30 -0500

    Refactor prompt code

--- builder.ml
+++ builder.ml
@@ -50,41 +50,37 @@ let build_quiz fname title desc sub cats num_qs =
   Yojson.Basic.pretty_to_channel file j;
   close_out file
 
-(** [prompt_name ()] is the file name of a new .quiz file that does not already 
+(** [prompt_file ()] is the file name of a new .quiz file that does not already 
     exist in the directory  *)
-let rec prompt_name () = 
+let rec prompt_file () = 
   print_string [] "Enter new .quiz file name > ";
   let f_check = read_line () in
   if Sys.file_exists ("."^Filename.dir_sep^"quizzes"^
                       Filename.dir_sep^ f_check^".quiz") = false then f_check 
   else let () = print_string [yellow] 
            ("Sorry, a quiz with that filename already exists!\n") in
-    prompt_name () 
+    prompt_file () 
+
+(** [prompt_string msg] returns user input after printing [msg]. *)
+let prompt_string msg = print_string [] msg; read_line ()
 
 (* [builder ()] prompts the user for details and executes the quiz builder *)
 let builder () =
   print_string [Bold] "Create an ASKYQuiz!\n";
-  let fname = prompt_name () in
-  print_newline ();
-  print_string [] "Enter quiz title > ";
-  let title = read_line () in
-  print_newline ();
-  print_string [] "Enter quiz description > ";
-  let desc = read_line () in
-  print_newline ();
-  print_string [] ("Will this quiz be (1) subjective e.g. personality quizzes "^
+  let fname = prompt_file () in
+  let title = prompt_string "\nEnter quiz title > " in
+  let desc  = prompt_string "\nEnter quiz description > " in
+  print_string [] ("\nWill this quiz be (1) subjective e.g. personality quizzes "^
                    "or (2) non-subjective e.g. academic quizzes > ");
   let sub = if read_line () = "1" then true else false in
   print_newline ();
-  let cats_list = if sub = true then
-      let cats = (print_string [] ("What are the answer categories "^
-                                   "(comma-separated, no space after)? > ");
-                  read_line ();) in String.split_on_char ',' cats
+  let cats_list =
+    if sub = true
+    then let cats = prompt_string "What are the answer categories \
+                                  (comma-separated, no space after)? > "
+    in String.split_on_char ',' cats
     else ["correct"]; in
-  print_newline ();
-  print_string [] ("How many questions will this quiz have? > ");
-  let num_qs = read_line () in
-  print_newline (); 
+  let num_qs = prompt_string "\nHow many questions will this quiz have? > " in
   build_quiz fname title desc sub cats_list num_qs;
   ignore(Unix.system ("vim ."^Filename.dir_sep^"quizzes"^
                       Filename.dir_sep^ fname^".quiz"))
