08751436e58e885f59202ac8284a94e31baf6380
Aidan Fitzgerald <acf67@cornell.edu>
2018-11-07 14:51:10 -0500

    Use edit distance to break ties

--- validation.ml
+++ validation.ml
@@ -12,11 +12,31 @@ let get_aid ltr options =
 let substr needle haystack =
   Str.string_match (Str.regexp_string needle) haystack 0
 
+(** [edit_dist str1 str2] is the edit distance between [str1] and [str2],
+    assuming that one of them is a substring of the other. *)
+let edit_dist str1 str2 =
+  abs ((String.length str1) - (String.length str2))
+
+(** [all_shortest_dist lst] is the list of ids that all have the shortest edit
+    distance, where [lst] is a list of [(id, dist)] pairs. *)
+let all_shortest_dist lst =
+  let sorted = List.fast_sort 
+                (fun (_, dist1) (_, dist2) -> compare dist1 dist2) lst in
+  let rec get_all_min = function
+    | [] -> []
+    | item :: [] -> [item]
+    | (id1, n1) :: (_, n2) :: _ when n1 <> n2 -> [(id1, n1)]
+    | (id1, n1) :: (id2, n2) :: tail ->
+      (id1, n1) :: get_all_min ((id2, n2) :: tail)
+  in
+  sorted |> get_all_min |> List.map fst
+
 let match_answer_text input options =
   let input_norm = normalize input in
   options |> List.map (fun (_, (id, text)) -> (id, normalize text))
           |> List.filter (fun (id, text) -> substr input_norm text)
-          |> List.map fst
+          |> List.map (fun (id, text) -> (id, edit_dist input_norm text))
+          |> all_shortest_dist
 
 let user_answer input options =
   match String.length input with
